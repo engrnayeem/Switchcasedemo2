b9ab6a40d96651dfb2358f82e10af164655b5be4
unknown <engr.mohammadjannatulnayeem@gmail.com>
2022-10-18 08:30:47 +0600

    code uploaded

---
+++ src/Switchcase2/SwitchCase2.java
@@ -0,0 +1,37 @@
+
+package Switchcase2;
+
+
+public class SwitchCase2 {
+    
+    public static void main(String[]args)
+    {
+        
+        int x=10;
+        
+        switch(x)
+        {
+        
+            case 2:
+                System.out.println("case 2");
+                break;
+                
+            case 10:
+                System.out.println("case 10");
+                break;
+            
+            default:
+                System.out.println("default");
+                break;
+        
+        
+        
+        }
+    
+    
+    
+    
+    }
+            
+    
+}
